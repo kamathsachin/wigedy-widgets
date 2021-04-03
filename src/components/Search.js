8289ff103fac456ed44ca0e476930bcb7b27d988
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-03 16:07:40 -0500

    Linked article to Wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -26,6 +26,14 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`http://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           {result.snippet}
