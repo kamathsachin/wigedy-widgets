941f4aa18d48c39e51b6861647de5d116623bafc
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-03 15:56:30 -0500

    added search component and integrated with wikipedia api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
-import Accordian from "./components/Accordian";
+// import Accordian from "./components/Accordian";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a front-end Javascript Framework",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a JS library among engineers",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components",
-  },
-];
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front-end Javascript Framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a JS library among engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ];
 
 const App = () => {
   return (
     <div>
-      <br />
-      <Accordian items={items} />
+      {/* <Accordian items={items} /> */}
+      <Search />
     </div>
   );
 };
---
+++ src/components/Search.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("Programming");
+  const [results, setResult] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResult(data.query.search);
+    };
+
+    search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          ></input>
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
