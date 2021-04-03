ea7c485808cf2a9a5adfe35552bea4f1f4d12416
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-03 10:25:30 -0500

    made changes to expand/collapse the accordian

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordian = ({ items }) => {
-  const renderedList = items.map((item) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const renderedList = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon" />
           {item.title}
         </div>
-        <div className="contect active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
