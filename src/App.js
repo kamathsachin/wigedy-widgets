0539f1c29a6557078799748e5b454960989c3096
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-04 15:36:53 -0500

    Added dropdown options using state implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordian from "./components/Accordian";
 // import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
@@ -34,11 +34,16 @@ const options = [
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       {/* <Accordian items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectionChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
